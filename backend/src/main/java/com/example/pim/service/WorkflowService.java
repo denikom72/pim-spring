6e2c4b24d506373a8ce4f6e32c2ec09f92e38939
root <root@localhost>
2022-09-05 23:01:55 +0200

    release 4.1.0

--- backend/src/main/java/com/example/pim/service/WorkflowService.java
+++ backend/src/main/java/com/example/pim/service/WorkflowService.java
@@ -1,27 +1,36 @@
 package com.example.pim.service;
 
 import com.example.pim.domain.Product;
+import com.example.pim.domain.Role;
 import com.example.pim.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class WorkflowService {
 
     private final ProductRepository productRepository;
     private final AuditLogService auditLogService;
+    private final NotificationService notificationService;
+    private final ReviewAssignmentService reviewAssignmentService;
 
-    // Define valid workflow transitions
+    // Define valid workflow transitions and required roles
     private static final List<String> VALID_WORKFLOW_STATUSES = Arrays.asList("not_started", "in_review", "approved", "rejected");
 
     @Autowired
-    public WorkflowService(ProductRepository productRepository, AuditLogService auditLogService) {
+    public WorkflowService(ProductRepository productRepository, AuditLogService auditLogService, NotificationService notificationService, ReviewAssignmentService reviewAssignmentService) {
         this.productRepository = productRepository;
         this.auditLogService = auditLogService;
+        this.notificationService = notificationService;
+        this.reviewAssignmentService = reviewAssignmentService;
     }
 
     @Transactional
@@ -33,14 +42,56 @@ public class WorkflowService {
             throw new IllegalArgumentException("Invalid workflow status: " + newWorkflowStatus);
         }
 
-        // TODO: Implement role-based permissions for state transitions
+        // Role-based permissions for state transitions
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Set<String> userRoles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .map(s -> s.replace("ROLE_", ""))
+                .collect(Collectors.toSet());
+
+        String currentStatus = product.getWorkflowStatus();
+
+        // Example permission logic (can be made more sophisticated)
+        if ("in_review".equals(newWorkflowStatus)) {
+            if (!userRoles.contains(Role.EDITOR.name()) && !userRoles.contains(Role.ADMIN.name())) {
+                throw new SecurityException("Unauthorized: Only EDITOR or ADMIN can send products for review.");
+            }
+        } else if ("approved".equals(newWorkflowStatus)) {
+            if (!userRoles.contains(Role.REVIEWER.name()) && !userRoles.contains(Role.ADMIN.name())) {
+                throw new SecurityException("Unauthorized: Only REVIEWER or ADMIN can approve products.");
+            }
+            if (!"in_review".equals(currentStatus)) {
+                throw new IllegalArgumentException("Product must be in 'in_review' status to be approved.");
+            }
+            // Validate reviewer assignments and approvals
+            long pendingApprovals = reviewAssignmentService.countPendingApprovals(productId);
+            if (pendingApprovals > 0) {
+                throw new IllegalArgumentException("Cannot approve product: " + pendingApprovals + " pending review(s) remaining.");
+            }
+        } else if ("rejected".equals(newWorkflowStatus)) {
+            if (!userRoles.contains(Role.REVIEWER.name()) && !userRoles.contains(Role.ADMIN.name())) {
+                throw new SecurityException("Unauthorized: Only REVIEWER or ADMIN can reject products.");
+            }
+            if (!"in_review".equals(currentStatus)) {
+                throw new IllegalArgumentException("Product must be in 'in_review' status to be rejected.");
+            }
+        }
 
         String oldWorkflowStatus = product.getWorkflowStatus();
         product.setWorkflowStatus(newWorkflowStatus);
         Product updatedProduct = productRepository.save(product);
 
         auditLogService.log("UPDATE_WORKFLOW_STATUS", "Product", updatedProduct.getId(), username);
-        // TODO: Implement notifications
+        try {
+            notificationService.sendNotification(username, "Product Workflow Update: " + updatedProduct.getName(),
+                    "Product '" + updatedProduct.getName() + "' workflow status changed from '" + oldWorkflowStatus + "' to '" + newWorkflowStatus + "' by " + username + ".");
+        } catch (Exception e) {
+            System.err.println("Failed to send notification for product ID " + updatedProduct.getId() + ": " + e.getMessage());
+            // Log the exception, but don't rethrow to avoid disrupting the main workflow
+        }
         return updatedProduct;
     }
 }
+
+
+
